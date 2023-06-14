7bf28b1f2f61f4d4e4274e3dfe1f1d9482e52df8
tmpITK <torok.mark.patrik@gmail.com>
2023-06-14 18:46:12 +0200

    Add deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,9 +2,20 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-ethers";
 import "@openzeppelin/hardhat-upgrades";
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname + "/.env" });
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "we have a problem";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,17 @@
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
+  const box = await ethers.getContractFactory("Box");
+  const boxProxy = await upgrades.deployProxy(box, [42], {
+    initializer: "store",
   });
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log("BoxProxy deployed to:", boxProxy.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
