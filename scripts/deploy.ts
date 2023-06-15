63bc2e0114a1e7a2ce656933910ceab751231d53
tmpITK <torok.mark.patrik@gmail.com>
2023-06-15 17:53:25 +0200

    Add comment for typing issue

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,6 +2,8 @@ import { ethers, upgrades } from "hardhat";
 
 async function main() {
   const box = await ethers.getContractFactory("Box");
+
+  // there are some typechain issues with the upgradeProxy function
   const boxProxy = await upgrades.deployProxy(box, [42], {
     initializer: "store",
   });
--- scripts/upgrade.ts
+++ scripts/upgrade.ts
@@ -3,6 +3,7 @@ import { ethers, upgrades } from "hardhat";
 async function main() {
   const newBox = await ethers.getContractFactory("NewBox");
 
+  // there are some typechain issues with the upgradeProxy function
   const boxProxy2 = await upgrades.upgradeProxy(
     "0x885edfd5d508bd7f2c48ab557c415f251ee5ffe2",
     newBox
